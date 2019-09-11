0cc6cd8657532f7be1c3cc5bc0dc02d3fd207b8b
olfamoussaoui <moussaoui.olfa1@gmail.com>
2019-09-11 20:30:58 +0200

    popup-fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalService } from './_modal/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'modal-app';
+  constructor(private modalService: ModalService) { }
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
